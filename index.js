7c892ef66b000b161f071eed5841a5331aacc6a2
Liz Murphy <jabuka@aol.com>
2018-01-14 04:18:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logshout(string) {
+  return string.toUpperCase()
+}
 function sayHiToGrandma(string) {
   return "I love you, too."
 }
