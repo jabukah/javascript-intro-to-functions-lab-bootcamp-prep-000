95c75e483ee3e58d41c0491e405bc3367e25bea4
Liz Murphy <jabuka@aol.com>
2018-01-14 06:33:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function sayHiToGrandma(string){
 	if(string.toUpperCase === string){
 		return 'YES INDEED!'
 	}
-	else if (other validation){
-		return 'other thing'
+	else if (string.toLowerCase ===string){
+		return 'I can\'t hear you'
 	}
 	else if (other validation){
 		return 'other thing'
